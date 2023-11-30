93a982543609f393caa310fe4ea8b7c487895ceb
Adem Şerif Dur <85637636+Ademseriff@users.noreply.github.com>
2023-11-30 21:05:57 +0300

    last changes

--- WebApplication2/Controllers/AccountController.cs
+++ WebApplication2/Controllers/AccountController.cs
@@ -1,10 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NETCore.Encrypt.Extensions;
+using WebApplication2.Entities;
 using WebApplication2.Models;
+using static WebApplication2.Entities.User;
 
 namespace WebApplication2.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly DataBaseContext _dataBaseContext;
+        private readonly IConfiguration _IConfiguration;
+
+        
+        public AccountController(DataBaseContext dataBaseContext , IConfiguration ıConfiguration)
+        {
+            _dataBaseContext = dataBaseContext;
+            _IConfiguration = ıConfiguration;
+        }
+
         public IActionResult Login()
         {
             return View();
@@ -14,7 +28,24 @@ namespace WebApplication2.Controllers
         {
             if (ModelState.IsValid)
             {
-                // login işlemleri
+                String md5salt = _IConfiguration.GetValue<string>("AppSettings:MD5Salt");
+                String saltedpassword = model.Password + md5salt;
+                string hashedpassword = saltedpassword.MD5();
+                User user = _dataBaseContext.Users.FirstOrDefault(x => x.Username.ToLower() == model.Username.ToLower() && x.Password == hashedpassword);
+                if(user != null)
+                {
+                    if (user.Locked)
+                    {
+                        ModelState.AddModelError(model.Username, "this acount is locked");
+                        return View();
+                    }
+
+                }
+                else
+                {
+                    ModelState.AddModelError("", "username or password is false");
+                }
+
             }
             return View(model);
         }
@@ -32,8 +63,33 @@ namespace WebApplication2.Controllers
         {
             if (ModelState.IsValid)
             {
-                // login işlemleri
-            }
+                if(_dataBaseContext.Users.Any(x => x.Username.ToLower() == model.Username.ToLower() ))
+                {
+                    Console.WriteLine("kullanıcı var ");
+                    return View(model);
+                }
+                String md5salt = _IConfiguration.GetValue<string>("AppSettings:MD5Salt");
+                String saltedpassword = model.Password + md5salt;
+                string hashedpassword = saltedpassword.MD5();
+
+                User user = new()
+                {
+                    Username = model.Username,
+                    Password = hashedpassword
+                };
+                _dataBaseContext.Add(user);
+               int count= _dataBaseContext.SaveChanges();//save changes değeri eğer başarılı olursa bir döndürür.
+
+                if (count == 0)
+                {
+                    Console.WriteLine("işlem başarı ile tamamlanamadı.");
+                }
+                else
+                {
+                    RedirectToAction(nameof(Login));
+                }
+                
+             }
             return View(model);
 
            
