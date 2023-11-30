8bbe58297351a8aa5419355d96b740e7c1c85570
Adem Şerif Dur <85637636+Ademseriff@users.noreply.github.com>
2023-11-30 14:57:10 +0300

    Add project files.

---
+++ WebApplication2/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // login işlemleri
+            }
+            return View(model);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="user name is required")]
+        [StringLength(30,ErrorMessage = "ussername can be max 30 characters") ]
+        public String Username { get; set; }
+        [Required]
+        [MinLength(6)]
+        [MaxLength(16)]
+        public String Password { get; set; }
+    }
+}
